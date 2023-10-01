4ed315754b21a882b565ef332ea547810043e359
ThousandDevils <c32flag@gmail.com>
2023-10-01 10:32:03 +0300

    added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+string getResult(int number){
+    string res = "";
+    if (num < 100) {
+        return "Третья цифра отсутствует";
+    }
+    while(num > 99) {
+        if (num > 999) {        
+            num = num / 10;
+        } else {
+            num = num % 10;
+            res = $"Третья цифра {num}";
+        }
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = getResult(num);
+Console.WriteLine(result);
