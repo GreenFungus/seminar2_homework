efd2aee406af676f4208daf5f4fc427e07dd4d5a
ThousandDevils <c32flag@gmail.com>
2023-10-01 10:00:26 +0300

    added Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+int GetNumber() {
+    Console.Write("Введите трёхзначное число:");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+int num = GetNumber();
+if (num < 100 || num > 999) {
+    Console.WriteLine("Число не трехзначное");
+    return;
+}
+Console.WriteLine($"Введенное число {num}");
+int secondRank = num / 10 % 10;
+Console.WriteLine($"Вторая цифра в этом числе {secondRank}");
+
+
